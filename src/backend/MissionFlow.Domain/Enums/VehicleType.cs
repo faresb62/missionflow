d2ba6711af955f4fd871641713ba5fb66e3b17ff
faresb62 <faresb351@gmail.com>
2026-07-29 08:15:49 -0500

    Add enums

---
+++ src/backend/MissionFlow.Domain/Enums/VehicleType.cs
@@ -0,0 +1,14 @@
+namespace MissionFlow.Domain.Enums;
+
+/// <summary>
+/// Consents origination: Manualiem.
+/// </summary>
+public enum VehicleType
+{
+    PersonalCar,
+    CompanyCar,
+    Requisitioned,
+    Train,
+    Plane,
+    Other
+}
