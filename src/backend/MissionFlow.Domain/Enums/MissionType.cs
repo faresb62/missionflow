e3247e8e785242b9bd92c5879710ae2efc59cd74
faresb62 <faresb351@gmail.com>
2026-07-29 08:19:51 -0500

    Add MissionType enum

---
+++ src/backend/MissionFlow.Domain/Enums/MissionType.cs
@@ -0,0 +1,15 @@
+namespace MissionFlow.Domain.Enums;
+
+/// <summary>
+/// Types of missions supported.
+/// </summary>
+public enum MissionType
+{
+    National = 1,
+    International = 2,
+    Training = 3,
+    Conference = 4,
+    ClientVisit = 5,
+    SiteInspection = 6,
+    Other = 7
+}
